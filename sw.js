62f7b65412ad7b8770ffc559ac74372613313979
Nguyen Thanh Toan <thanhtoan.mirats@ms365vip.com>
2026-09-21 07:30:21 +0700

    Deploy latest AeroScribe build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "aeroscribe-v1";
+const CACHE = "aeroscribe-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
